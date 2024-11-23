8928573bf35fa8e4022c4e53aff004026a4d3073
keshav1235gupta@gmail.com <keshav1235gupta@gmail.com>
2024-11-23 18:07:10 +0530

    Initial stup with frontend and backend folders

---
+++ backend/jobsphere.api/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jobsphere.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        [Route("signup")]
+        public async Task<IActionResult> signup()
+        {
+            return Ok("You hit the signup endpoint");
+        }
+    }
+}
