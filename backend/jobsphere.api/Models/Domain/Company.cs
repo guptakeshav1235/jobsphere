1152f6b1575c822bc3bf8462294bf621de12d84b
keshav1235gupta@gmail.com <keshav1235gupta@gmail.com>
2024-11-23 21:43:46 +0530

    [Backend] - All necessary models have implemented

---
+++ backend/jobsphere.api/Models/Domain/Company.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jobsphere.api.Models.Domain
+{
+    public class Company
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Website {  get; set; }
+        public string Location {  get; set; }
+        public string Logo {  get; set; }//URL to Company Logo
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        public DateTime CreatedAt { get; set; } = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("India Standard Time"));
+        public DateTime UpdatedAt { get; set; } = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, TimeZoneInfo.FindSystemTimeZoneById("India Standard Time"));
+    }
+}
---
+++ backend/jobsphere.api/Models/Domain/UserProfile.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace jobsphere.api.Models.Domain
+{
+    public class UserProfile
+    {
+        public Guid Id { get; set; }=Guid.NewGuid();
+        public string Bio {  get; set; }
+        public List<string> Skills { get; set; }=new List<string>();
+        public string Resume {  get; set; }//URL to Resume File
+        public string ResumeOriginalName { get; set; }
+        public string ProfilePhoto {  get; set; }=string.Empty;
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+    }
+}
