b5d3bc067867a5c5bbffbe1c3693f1fe5c901d4a
keshav1235gupta@gmail.com <keshav1235gupta@gmail.com>
2024-11-27 11:57:14 +0530

    [Backend] - Job Controller have been implemented

---
+++ backend/jobsphere.api/Controllers/JobController.cs
@@ -0,0 +1,80 @@
+﻿using AutoMapper;
+using jobsphere.api.CustomValidation;
+using jobsphere.api.Models.Domain;
+using jobsphere.api.Models.DTO;
+using jobsphere.api.Repository.JobRepo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jobsphere.api.Controllers
+{
+    [Route("api/job")]
+    [ApiController]
+    [ServiceFilter(typeof(IsAuthenticatedAttribute))]
+    public class JobController : ControllerBase
+    {
+        private readonly IJobRepository jobRepository;
+        private readonly IMapper mapper;
+
+        public JobController(IJobRepository jobRepository, IMapper mapper)
+        {
+            this.jobRepository = jobRepository;
+            this.mapper = mapper;
+        }
+        [HttpPost("post")]
+        public async Task<IActionResult> PostJob([FromBody] PostJobDto postJobDto)
+        {
+            var userId = HttpContext.Items["UserId"] as string;
+
+            //Mapping Dto->Domain
+            var job = mapper.Map<Job>(postJobDto);
+            job.CreatedById = Guid.Parse(userId);
+
+            var postJob = await jobRepository.PostJobAsync(job);
+
+            //return Ok after mapping Domain->Dto
+            return Ok(mapper.Map<JobResponseDto>(postJob));
+        }
+
+        [HttpGet("get")]
+        public async Task<IActionResult> GetAllJobs([FromQuery] string keyword = "")
+        {
+            var jobs = await jobRepository.GetAllJobsAsync(keyword);
+            if(jobs==null || !jobs.Any())
+            {
+                return NotFound(new { error = "Jobs not found" });
+            }
+
+            //return ok after mapping domain->dto
+            return Ok(mapper.Map<IEnumerable<JobResponseDto>>(jobs));
+        }
+
+        [HttpGet("get/{id:Guid}")]
+        public async Task<IActionResult> GetJobById([FromRoute] Guid id)
+        {
+            var job = await jobRepository.GetJobByIdAsync(id);
+            if (job == null)
+            {
+                return NotFound(new { error = "Job not found" });
+            }
+
+            //return Ok after mapping Domain->Dto
+            return Ok(mapper.Map<JobResponseDto>(job));
+        }
+
+        [HttpGet("getadminjobs")]
+        public async Task<IActionResult> GetAdminJobs()
+        {
+            var adminId = HttpContext.Items["UserId"] as string;
+            var jobs = await jobRepository.GetAdminJobsAsync(Guid.Parse(adminId));
+
+            if (jobs == null || !jobs.Any())
+            {
+                return NotFound(new { error = "Jobs not found" });
+            }
+
+            //return Ok after mapping Domain->Dto
+            return Ok(mapper.Map<IEnumerable<JobResponseDto>>(jobs));
+        }
+    }
+}
---
+++ backend/jobsphere.api/Repository/JobRepo/IJobRepository.cs
@@ -0,0 +1,12 @@
+﻿using jobsphere.api.Models.Domain;
+
+namespace jobsphere.api.Repository.JobRepo
+{
+    public interface IJobRepository
+    {
+        Task<Job> PostJobAsync(Job job);
+        Task<IEnumerable<Job>> GetAllJobsAsync(string keyword);
+        Task<Job> GetJobByIdAsync(Guid jobId);
+        Task<IEnumerable<Job>> GetAdminJobsAsync(Guid adminId);
+    }
+}
