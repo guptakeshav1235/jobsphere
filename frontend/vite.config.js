1bdf699680e9f495a227d9ef5dbbca11dbaea775
keshav1235gupta@gmail.com <keshav1235gupta@gmail.com>
2024-12-03 21:51:17 +0530

    [Frontend] - Login and Signup Page have been implemented

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv';
+dotenv.config();
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000
+    port: 3000,
+    proxy: {
+      '/url': {
+        target: process.env.VITE_API_URL,
+        changeOrigin: true,
+        rewrite: (path) => {
+          console.log(`Proxying request: ${path}`); // Log proxy paths
+          return path.replace(/^\/url/, '');
+        },
+      }
+    }
   }
 })
